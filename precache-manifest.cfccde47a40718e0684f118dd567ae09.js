4b09963855ab44a9adf2a55151eaf4c86a0d7485
Sergio Garcia Mondaray <sgmonda@gmail.com>
2021-03-23 10:02:56 +0100

    Updates

---
+++ precache-manifest.cfccde47a40718e0684f118dd567ae09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25bdc7adfc88e71b4b60c2fa3226d75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "beb7f8c21898ee949263",
+    "url": "./static/css/main.ba255de2.chunk.css"
+  },
+  {
+    "revision": "0b32ee205fa9a912e36c",
+    "url": "./static/js/2.6f43b382.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6f43b382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb7f8c21898ee949263",
+    "url": "./static/js/main.720f7728.chunk.js"
+  },
+  {
+    "revision": "f7af4d8f21e8c5544a05",
+    "url": "./static/js/runtime-main.1a20905b.js"
+  }
+]);
