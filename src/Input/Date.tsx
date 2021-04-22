531b5cc0d9c414e6796f9c1a9e4a4cd33d38714c
Sergio Garcia Mondaray <sgmonda@gmail.com>
2021-04-22 10:46:49 +0200

    build: try to use react-datepicker css without external import

--- src/Input/Date.tsx
+++ src/Input/Date.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import './Date.css';
 import styles from '../styles.module.css';
 import { createGlobalStyle } from 'styled-components';
 import { TInputProps } from '../types';
