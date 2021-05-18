88532e6de9dcf0108420077e3dd04d4c098cc929
Sergio Garcia Mondaray <sgmonda@gmail.com>
2021-05-18 11:34:49 +0200

    fix: keep caret position on textarea

--- src/Input/TextArea.tsx
+++ src/Input/TextArea.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler, useEffect, useRef, useState } from 'react';
 import cl from 'classnames';
 import { TInputProps } from '../types';
 import styles from '../style/index.module.sass';
 
 const TextArea = (props: TInputProps<string>) => {
+  const [caretPosition, setCaretPosition] = useState<number>(props.value?.length || 0);
+  const ref = useRef<HTMLTextAreaElement | null>(null);
+
+  const onChange: ChangeEventHandler<any> = (ev: ChangeEvent<HTMLTextAreaElement>) => {
+    setCaretPosition(ref?.current?.selectionStart || props.value?.length || 0);
+    props.onChange(ev.target.value);
+  };
+
+  useEffect(() => {
+    try {
+      ref?.current?.setSelectionRange(caretPosition, caretPosition);
+    } catch (err) {
+      console.warn(`Selection range cannot be set`);
+    }
+  }, [props.value]);
+
   return (
     <textarea
       id={props.id}
+      ref={ref}
       required={props.required}
       disabled={props.disabled}
       name={props.name}
@@ -19,7 +36,7 @@ const TextArea = (props: TInputProps<string>) => {
       value={props.value || ''}
       onFocus={props.onFocus}
       onBlur={props.onBlur}
-      onChange={(ev) => props.onChange(ev.target.value)}
+      onChange={onChange}
     ></textarea>
   );
 };
