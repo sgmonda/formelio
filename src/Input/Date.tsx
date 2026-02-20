92f992c337a3b94d364d5c589c7e642290fec375
Sergio García Mondaray <sgmonda@gmail.com>
2026-02-20 23:07:28 +0100

    ci: fixed publish

--- src/Input/Date.tsx
+++ src/Input/Date.tsx
@@ -1,6 +1,6 @@
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import './Date.module.css';
+import './Date.css';
 import styles from '../style/index.module.sass';
 import { createGlobalStyle } from 'styled-components';
 import { TInputProps } from '../types';
